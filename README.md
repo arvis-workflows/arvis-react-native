877014a3e298fa64812fe3c8769016c81b7c8667
jopemachine <jopemachine@naver.com>
2021-06-08 01:36:41 +0900

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# arvis-react-native
+> [Arvis](https://github.com/jopemachine/arvis) workflow to access the React Native Documentation
+
+![screenshot.png](screenshot.png)
+
+## Install
+
+```
+npm i -g arvis-react-native
+```
+
+## Usage
+
+In arvis, type rn, Enter, and your query, to search for documentation of components.
+Select a component and press Enter to go to its GitHub repo.
+
+## Thanks
+
+Thanks to [sindresorhus](https://github.com/sindresorhus) for making [alfy](https://github.com/sindresorhus/alfy). It works great - made this workflow in 15 minutes.
---
+++ index.js
@@ -0,0 +1,169 @@
+const arvish = require('@jopemachine/arvish');
+
+const rn_website = 'https://facebook.github.io/react-native/docs/';
+const toc = [
+  {
+    title: 'ActivityIndicator',
+    url: 'activityindicator'
+  },
+  {
+    title: 'Button',
+    url: 'button'
+  },
+  {
+    title: 'DatePickerIOS',
+    url: 'datepickerios'
+  },
+  {
+    title: 'DrawerLayoutAndroid',
+    url: 'drawerlayoutandroid'
+  },
+  {
+    title: 'FlatList',
+    url: 'flatlist'
+  },
+  {
+    title: 'Image',
+    url: 'image'
+  },
+  {
+    title: 'InputAccessoryView',
+    url: 'inputaccessoryview'
+  },
+  {
+    title: 'KeyboardAvoidingView',
+    url: 'keyboardavoidingview'
+  },
+  {
+    title: 'ListView',
+    url: 'listview'
+  },
+  {
+    title: 'MapView',
+    url: 'mapview'
+  },
+  {
+    title: 'MaskedViewIOS',
+    url: 'maskedviewios'
+  },
+  {
+    title: 'Modal',
+    url: 'modal'
+  },
+  {
+    title: 'Navigator',
+    url: 'navigator'
+  },
+  {
+    title: 'NavigatorIOS',
+    url: 'navigatorios'
+  },
+  {
+    title: 'PickerIOS',
+    url: 'pickerios'
+  },
+  {
+    title: 'Picker',
+    url: 'picker'
+  },
+  {
+    title: 'ProgressBarAndroid',
+    url: 'progressbarandroid'
+  },
+  {
+    title: 'ProgressViewIOS',
+    url: 'progressviewios'
+  },
+  {
+    title: 'RefreshControl',
+    url: 'refreshcontrol'
+  },
+  {
+    title: 'SectionList',
+    url: 'sectionlist'
+  },
+  {
+    title: 'ScrollView',
+    url: 'scrollview'
+  },
+  {
+    title: 'SegmentedControlIOS',
+    url: 'segmentedcontrolios'
+  },
+  {
+    title: 'Slider',
+    url: 'slider'
+  },
+  {
+    title: 'StatusBar',
+    url: 'statusbar'
+  },
+  {
+    title: 'Switch',
+    url: 'switch'
+  },
+  {
+    title: 'TabBarIOS',
+    url: 'tabbarios'
+  },
+  {
+    title: 'TabBarIOS.Item',
+    url: 'tabbarios-item'
+  },
+  {
+    title: 'Text',
+    url: 'text'
+  },
+  {
+    title: 'TextInput',
+    url: 'textinput'
+  },
+  {
+    title: 'ToolbarAndroid',
+    url: 'toolbarandroid'
+  },
+  {
+    title: 'TouchableHighlight',
+    url: 'touchablehighlight'
+  },
+  {
+    title: 'TouchableNativeFeedback',
+    url: 'touchablenativefeedback'
+  },
+  {
+    title: 'TouchableOpacity',
+    url: 'touchableopacity'
+  },
+  {
+    title: 'TouchableWithoutFeedback',
+    url: 'touchablewithoutfeedback'
+  },
+  {
+    title: 'View',
+    url: 'view'
+  },
+  {
+    title: 'ViewPagerAndroid',
+    url: 'viewpagerandroid'
+  },
+  {
+    title: 'VirtualizedList',
+    url: 'virtualizedlist'
+  },
+  {
+    title: 'WebView',
+    url: 'webview'
+  }
+];
+
+const output = arvish
+  .inputMatches(toc, 'title')
+  .map(doc => {
+    return {
+      title: doc.title,
+      arg: rn_website + doc.url + '.html',
+      subtitle: rn_website + doc.url + '.html'
+    };
+  });
+
+arvish.output(output);
